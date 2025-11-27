a600223d286a1bb79f80cb7886261bc46a4e4ce1
WDCT-Wren <wrenctulio@gmail.com>
2025-11-27 23:40:28 +0800

    Made a UI Class, and made the data structure of the fixed rates of the program as well as the method for computing the tenant's electricity bill.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,45 @@
+import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     final static Scanner sc = new Scanner(System.in);
-    static Object[][] fixedBills = {
-        {"Monthly Rent", "Water Bill", "Internet Bill" },
-        {4500, 150, 320}
-    };
+
     public static void main(String[] args) {
-        System.out.println("Will there be any changes in the fixed bills? (y/n)>>");
-        String answer = sc.nextLine();
-        if (answer.equalsIgnoreCase("y")) {} //moves forward to the dynamic billing
-        else {changeFixedVariables();} //proceed to the section where the user can change the values of the fixed variables
+        UI.programBanner();
+        double electricityBill = electricityComputer();
+        System.out.println(electricityBill);
     }
 
-    public static void changeFixedVariables() {
-        //#TODO: make the method that makes it possible to change the arrays.
+    public static void fixedBills() {
+        HashMap<String, Double> fixedBills = new HashMap<>();
+
+        //Fixed Bills that will be added to the total rent for every month. 
+        fixedBills.put("Monthly Rent",  4500.0);
+        fixedBills.put("Water Bill",  150.0);
+        fixedBills.put("Internet Bill",  320.0);
+    }
+
+    public static double electricityComputer() {
+        double prevReading = 0.0;
+        double presentReading = 0.0;
+        double ratePerKW = 0.0;
+        try {
+            System.out.print("ENTER PRESENT ELECTRICITY METER READING>> ");
+            presentReading = sc.nextDouble();
+
+            System.out.print("ENTER PREVIOUS ELECTRICITY METER READING>> ");
+            prevReading = sc.nextDouble();
+            double kiloWattConsumed = presentReading - prevReading;
+
+            System.out.print("ENTER THIS MONTH'S RATE PER KILLOWATT OF ELECTRICITY>>");
+            ratePerKW = sc.nextDouble();
+            double electricityBill = ratePerKW * kiloWattConsumed;
+            return electricityBill;
+        }
+        catch (InputMismatchException e) {
+            //TODO: finalize the excpetion handler
+        }
+        return 0;
     }
 }
---
+++ src/UI.java
@@ -0,0 +1,16 @@
+public class UI {
+    static void programBanner() {
+        System.out.println("""
+                OooOOo.                                              `OooOOo.                             o 
+                O     `O                                              o     `o                           O  
+                o      O                                   O          O      O                O          o  
+                O     .o                                  oOo         o     .O               oOo         O  
+                oOooOO'  `OoOo. .oOo. .oOo. .oOo. `OoOo.   o   O   o  OOooOO'  .oOo. 'OoOo.   o   .oOoO' o  
+                o         o     O   o O   o OooO'  o       O   o   O  o    o   OooO'  o   O   O   O   o  O  
+                O         O     o   O o   O O      O       o   O   o  O     O  O      O   o   o   o   O  o  
+                o'        o     `OoO' oOoO' `OoO'  o       `oO `OoOO  O      o `OoO'  o   O   `oO `OoO'o Oo 
+                                    O                            o                                        
+                                    o'                        OoO'                                        
+                """);
+    }
+}
